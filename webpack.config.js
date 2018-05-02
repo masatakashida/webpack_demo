d1411276f10fb5eff992703e2ca44723dfe79613
masataka <ar.tec.masa@gmail.com>
2018-05-03 01:06:47 +0900

    devServer

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,13 @@ module.exports = {
   mode: 'development',
   entry: `./src/index.js`,
 
-  //
   output: {
-  	//
   	path: `${__dirname}/dist`,
-  	//
   	filename: 'main.js'
   },
+
+  devServer: {
+    contentBase: 'dist',
+    open: true
+  }
 };
